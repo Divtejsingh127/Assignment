39d177ffdcf58903a4805bb7974f10e027f33a25
divte <divte@DESKTOP-EJN014L>
2024-02-05 13:34:58 -0500

    Add project files.

---
+++ Assignment/Pet.cs
@@ -0,0 +1,17 @@
+﻿namespace VirtualPet
+{
+    internal class Pet
+    {
+        private string? petType;
+        private string? petName;
+
+        public Pet(string? petType, string? petName)
+        {
+            this.petType = petType;
+            this.petName = petName;
+        }
+
+        public object PetType { get; internal set; }
+        public object PetName { get; internal set; }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Virtual Pet!");
+
+            Console.WriteLine("Choose a pet type (cat, dog, rabbit): ");
+            string petType = Console.ReadLine();
+            Console.WriteLine("Enter a name for your pet: ");
+            string petName = Console.ReadLine();
+
+            Pet myPet = new Pet(petType, petName);
+
+            Console.WriteLine($"Congratulations! You have a new {myPet.PetType} named {myPet.PetName}.");
+
+            bool exit = false;
+           
+         }
+    }
+}
+
